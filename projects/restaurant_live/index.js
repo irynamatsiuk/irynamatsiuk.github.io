9e2f8dbc2cf4e211d80629f348d876423a2d7bfe
Iryna Matsiuk <irynamatsiuk@users.noreply.github.com>
2024-11-22 14:02:18 +0100

    Run Prettier

--- projects/restaurant_live/index.js
+++ projects/restaurant_live/index.js
@@ -1,30 +1,26 @@
-
-// import "./styles.css";
-import  createHomeTab from "./home.js";
-import  createContactTab  from "./contact.js";
+import createHomeTab from "./home.js";
+import createContactTab from "./contact.js";
 import createMenuTab from "./menu.js";
 
-// Home Page 
-  // autoload
+// Home Tab
+// autoload
 document.addEventListener("DOMContentLoaded", (event) => {
-    createHomeTab();
+  createHomeTab();
 });
-  // load on click
+// load on click
 document.querySelector("#home-btn").addEventListener("click", (event) => {
-    document.querySelector("#content").replaceChildren();
-    createHomeTab();
+  document.querySelector("#content").replaceChildren();
+  createHomeTab();
 });
 
-// Contact Page load on click
+// Contact Tab load on click
 document.querySelector("#contact-btn").addEventListener("click", (event) => {
-    document.querySelector("#content").replaceChildren();
-    createContactTab()
+  document.querySelector("#content").replaceChildren();
+  createContactTab();
 });
 
-// Menu Page load on click
+// Menu Tab load on click
 document.querySelector("#menu-btn").addEventListener("click", (event) => {
-    document.querySelector("#content").replaceChildren();
-    createMenuTab()
-})
-
-
+  document.querySelector("#content").replaceChildren();
+  createMenuTab();
+});

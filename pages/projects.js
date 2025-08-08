5c0adb7f491ea6fabedc194d8e4f1892a3d56d27
Iryna Matsiuk <141046619+irynamatsiuk@users.noreply.github.com>
2025-08-08 13:11:12 +0200

    Refactor showProjects function

--- pages/projects.js
+++ pages/projects.js
@@ -3,9 +3,9 @@ import { filterCategories } from "./projectsFilterData.js";
 
 function showProjects() {
   const template = document.querySelector("#project");
+  const projectsContainer = document.querySelector(".projects");
 
-  for (let i = projects.length - 1; i >= 0; i--) {
-    const project = projects[i];
+  for (const project of [...projects].reverse()) {
     const clone = template.content.cloneNode(true);
     const projectEl = clone.querySelector(".project");
     const name = clone.querySelector(".name");
@@ -19,19 +19,21 @@ function showProjects() {
     name.textContent = project.name;
     details.textContent = project.details;
     courseName.textContent = `\u00A0 ${project.courseProvider}, ${project.courseName}`;
-    github.innerHTML = `<a href=${project.sourceLink} target=_"blank">Github</a>`;
+    github.innerHTML = `<a href=${project.sourceLink} target="_blank">Github</a>`;
 
-    if (project.liveLink != "") {
-      live.innerHTML = `<a href=${project.liveLink} target=_"blank">Live</a>`;
+    if (project.liveLink) {
+      live.innerHTML = `<a href=${project.liveLink} target="_blank">Live</a>`;
     }
 
-    project.description.map((paragraph) => {
-      let p = document.createElement("p");
-      p.textContent = paragraph;
-      description.appendChild(p);
-    });
+    if (Array.isArray(project.description)) {
+      project.description.forEach((paragraph) => {
+        const p = document.createElement("p");
+        p.textContent = paragraph;
+        description.appendChild(p);
+      });
+    }
 
-    document.querySelector(".projects").appendChild(clone);
+    projectsContainer.appendChild(clone);
   }
 }
 

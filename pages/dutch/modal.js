1acf6647c49e381eae1ccd3664f78169cb19c5aa
Iryna Matsiuk <141046619+irynamatsiuk@users.noreply.github.com>
2025-08-16 18:57:23 +0200

    Refactor modal

--- pages/dutch/modal.js
+++ pages/dutch/modal.js
@@ -2,10 +2,24 @@ const modal = document.querySelector("#modalOverlay");
 const continueBtn = document.querySelector("#continueBtn");
 let targetUrl;
 
+// Prevent stuck modal on refresh
+window.addEventListener("load", () => {
+  modal.style.display = "none";
+});
+
 document.querySelectorAll(".needs-modal").forEach((link) => {
   link.addEventListener("click", function (event) {
+    const href = this.href;
+    const storageKey = `shownModalFor:${href}`;
+
+    // If modal already shown for this link, go directly to link
+    if (sessionStorage.getItem(storageKey)) {
+      return;
+    }
+
     event.preventDefault();
-    targetUrl = this.href;
+    targetUrl = href;
+    sessionStorage.setItem(storageKey, "true");
     modal.style.display = "flex";
   });
 });

5aac86626ee70966b2d1587bf87e26e88c41b79e
Iryna Matsiuk <141046619+irynamatsiuk@users.noreply.github.com>
2025-11-28 11:25:25 +0100

    Add a 500ms timeout to server loading show spinner

--- projects/blognooneasked/adminFrontend/assets/js/utils/wakeUpServer.js
+++ projects/blognooneasked/adminFrontend/assets/js/utils/wakeUpServer.js
@@ -2,12 +2,16 @@ import { getCurrentUser } from "../api/auth.js";
 
 export async function wakeUpServer() {
   const box = document.getElementById("server-wakeup");
-  box?.classList.remove("hidden");
+
+  const showSpinnerTimeout = setTimeout(() => {
+    box?.classList.remove("hidden");
+  }, 500);
 
   try {
     const user = await getCurrentUser();
     return user || null;
   } finally {
+    clearTimeout(showSpinnerTimeout); // cancel the show of spinner if the server responses faster than 500ms
     box?.classList.add("hidden");
   }
 }
